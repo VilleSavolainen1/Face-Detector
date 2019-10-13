e079124b85a0c2ac867e571145953f45e6280853
Ville Savolainen <visavola@gmail.com>
2019-10-13 15:47:03 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react';
-//import Navigation from './components/Navigation/Navigation';
-//import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
-//import Rank from './components/Rank/Rank';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-//import SignIn from './components/SignIn/SignIn';
-//import Register from './components/Register/Register';
 import './index.css';
 import Clarifai from 'clarifai'
 
@@ -59,39 +54,16 @@ onButtonSubmit = () => {
     .catch(err => console.log(err));
 }
 
-/*onRouteChange = (route) => {
-  if(route === 'signout') {
-    this.setState({isSignedIn: false})
-  } else if ( route === 'home') {
-    this.setState({isSignedIn: true})
-  }
-  this.setState({route: route});
-}*/
-
   render() {
     const { isSignedIn, route, box, imageUrl } = this.state;
   return (
     <div className="App">
-      {/*<Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
-      { route === 'home'
-       ? 
-       <div>
-      {/*<Logo />
-      <Rank />*/}
       <ImageLinkForm 
         onInputChange={this.onInputChange}
         onSubmit={this.onButtonSubmit}/>
       <FaceRecognition box={box} imageUrl={imageUrl} />
       </div>
       )
-      {/*: (
-        route === 'signin' 
-        ? <SignIn onRouteChange={this.onRouteChange} />
-        : <Register onRouteChange={this.onRouteChange}/>
-        )
-      }
-    </div>
-    );*/}
   }
 }
 
